56ebe31ea97ac4a7091249d94ffae99cbefea8e0
brunozarabr <59149143+brunozarabr@users.noreply.github.com>
2024-05-17 11:36:05 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,10 +1,13 @@
 #include "ui/ui.h"
+#include "core/kernel.h"
+#include "core/scheduler.c"
 #include <stdio.h>
 #include <stdlib.h>
-
+extern struct kernel kernel;
 int main(void)
 {
-    run_curses();
-    printf("bye :)\n");
+    kernel_init();
+    syscall(PROCESS_CREATE, "programs/p1");
+    kernel_run();
     return EXIT_SUCCESS;
 }
